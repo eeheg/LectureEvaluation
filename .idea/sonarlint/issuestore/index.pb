a255b8543260e5996e785715eaf7164915c81456
eeheg <sjh8924@naver.com>
2023-08-28 22:00:44 +0900

    commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+N
+src/main/webapp/css/custom.css,8\1\81f8cb455caa3d38314fcc3de2c34d709ff803c6
---
+++ src/main/java/util/Gmail.java
@@ -0,0 +1,19 @@
+package util;
+
+/*
+pom.xml에 외부라이브러리 넣기 (dependency)
+- javamail 1.4.7
+- javabeans activation framework(JAF) 1.1.1
+ */
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class Gmail extends Authenticator {
+
+    //나의 구글 아이디,비밀번호를 넣는다. 메일을 전송할 관리자의 Gmail계정.
+    @Override
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication("TESTsjh8924@gmail.com", "testtest**");
+    }
+
+}
